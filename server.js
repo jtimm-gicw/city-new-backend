ee7427307cae5d33d1a1c075c0879a199da44811
Jason <jtimm@gicw.org>
2025-04-10 14:10:14 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Load Environment Variables from the .env file
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+
+// Our Dependencies
+const weather = require('./modules/weather.js');
+
+// Application Setup
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
+
+app.get('/weather', weatherHandler);
+
+app.use('*', notFoundHandler);
+
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  console.log('in weather', lat, lon);
+  weather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(500).send('Sorry. Something went wrong!');
+    });
+}
+
+
+function notFoundHandler(request, response) {
+  response.status(404).send('huh?');
+}
+
+app.listen(PORT, () => console.log(`Server up on ${PORT}`));
