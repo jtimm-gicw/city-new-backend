d73f136d8c1866a94748589c5ad487e9cb7fc441
Jason <jtimm@gicw.org>
2026-06-01 13:15:17 -0700

    updated weather.js with icons

--- modules/weather.js
+++ modules/weather.js
@@ -7,7 +7,9 @@ module.exports = getWeather;
 
 function getWeather(latitude, longitude) {
   const key = 'weather-' + latitude + longitude;
-  const url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${latitude}&lon=${longitude}&days=5`;
+  console.log('WEATHER KEY:', process.env.WEATHER_API_KEY);
+  
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${latitude}&lon=${longitude}&days=5`;
 
   if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
     console.log('Cache hit');
@@ -23,20 +25,39 @@ function getWeather(latitude, longitude) {
 }
 
 function parseWeather(weatherData) {
+
   try {
+
     const weatherSummaries = weatherData.data.map(day => {
+
+      // DEBUGGING
+      console.log(day.weather);
+
       return new Weather(day);
+
     });
+
     return Promise.resolve(weatherSummaries);
+
   } catch (e) {
+
     return Promise.reject(e);
+
   }
+
 }
 
 class Weather {
   constructor(day) {
+
     this.forecast = day.weather.description;
+
     this.time = day.datetime;
+
+    // Weatherbit icon code
+    this.icon = day.weather.icon;
+
     this.timestamp = Date.now();
   }
 }
+
